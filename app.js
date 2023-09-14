d566d5b95ea33c99c422489535a840cf504bf41c
G-RIMA <gikandimaria178@gmail.com>
2023-09-14 21:55:52 +0300

    fixed code  in app.js

--- app.js
+++ app.js
@@ -107,6 +107,13 @@ app.delete("/api/:userId", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
