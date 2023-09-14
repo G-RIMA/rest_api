b6b0d4f83ee835434f56873eca21c785a7c4fba9
G-RIMA <gikandimaria178@gmail.com>
2023-09-14 20:57:24 +0300

    fixed code  in app.js

--- api/create.js
+++ api/create.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
   try {
     const { name, age } = req.body;
     const highestPerson = await Person.findOne({}, {}, { sort: { _id: -1 } });
-    const nextId = highestPerson ? highestPerson._id + 1 : 1;
+    const nextId = await getNextId();
     const person = new Person({ _id: nextId, name, age });
     await person.save();
     res.status(201).json(person);
