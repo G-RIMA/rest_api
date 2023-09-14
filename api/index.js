2ded0085ded4c211e6af3d3e58bb1ead42614ded
G-RIMA <gikandimaria178@gmail.com>
2023-09-14 22:08:31 +0300

    fix code

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Person = require("./models/person");
+const Person = require("../models/person");
 
 const app = express();
 app.use(bodyParser.json());
