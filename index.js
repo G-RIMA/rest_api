5d7326dce2f52fa7da2bed0bfa2eb0bca263b239
G-RIMA <gikandimaria178@gmail.com>
2023-09-15 07:59:37 +0300

    deploy

--- index.js
+++ index.js
@@ -106,6 +106,10 @@ app.delete("/api/:userId", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", async (req, res) => {
+  return "welcome to my rest api";
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
